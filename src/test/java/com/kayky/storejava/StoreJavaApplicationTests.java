c70027e3644e7f8d8acb05d5348703d5ec0edcb3
Kayky Matos Santana <kayky7277@gmail.com>
2022-05-17 09:04:37 -0300

    Project created

---
+++ src/test/java/com/kayky/storejava/StoreJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kayky.storejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
