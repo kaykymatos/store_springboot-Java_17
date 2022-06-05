31595f506e3678b8e4b65750439b5dd4163f4193
Kayky Matos Santana <kayky7277@gmail.com>
2022-06-04 22:03:28 -0300

    MySql implementation

--- src/main/java/com/kayky/storejava/services/UserService.java
+++ src/main/java/com/kayky/storejava/services/UserService.java
@@ -3,11 +3,17 @@ package com.kayky.storejava.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.kayky.storejava.entities.User;
 import com.kayky.storejava.repositories.UserRepository;
+import com.kayky.storejava.services.exceptions.DatabaseException;
+import com.kayky.storejava.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -20,7 +26,7 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	public User insert(User obj) {
@@ -28,13 +34,23 @@ public class UserService {
 	}
 
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 
 	public User update(Long id, User obj) {
-		User entity = repository.getById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
